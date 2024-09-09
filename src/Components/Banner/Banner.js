8ffe3ebc6921b82cc713212267b4baffdf820b4c
akshay c s <akshaycs0480@gmail.com>
2024-09-09 15:23:29 +0530

    project design completed

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Banner.css"
+function Banner() {
+  return (
+    <div className="banner">
+      <div className="content">
+        <h1 className="title">movie name</h1>
+        <div className="banner_button">
+          <button className="button">play</button>
+          <button className="button">my list</button>
+        </div>
+        <h1 className="description">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+        </h1>
+      </div>
+
+    <div className="fade">
+        
+    </div>
+
+    </div>
+  );
+}
+
+export default Banner;
